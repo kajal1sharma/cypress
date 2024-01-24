0681e934f6c40eb3fc21804581d4bb55481bfb8f
kajal <kajal@gmail.com>
2024-01-24 20:50:51 +0530

    ..

---
+++ WebTest/cypress/e2e/spec.cy.js
@@ -0,0 +1,43 @@
+describe('template spec 1', () => {
+  
+  it('passes 1', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('passes 2', () => {
+    cy.visit('https://www.google.co.in/')
+  })
+
+})
+
+// describe('template spec 2', () => {
+  
+//   it('passes', () => {
+//     cy.visit('http://127.0.0.1:5500/index.html')
+//   })
+
+  
+  
+// })
+
+
+
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
+// describe => test-suite//login PAge 
+//it => test// password inputbox => 8char @
+//it => //username => length =>4 color=>blue
---
+++ WebTest/cypress/e2e/spec2.cy.js
@@ -0,0 +1,11 @@
+describe('template spec 2', () => {
+  
+    it('passes3', () => {
+      cy.visit('https://example.cypress.io')
+    })
+  
+    it('passes4', () => {
+      cy.visit('https://www.google.co.in/')
+    })
+  
+  })
